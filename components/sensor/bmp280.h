1a0b0610027af790c5c9d0c146ed279c70fe7a43
Atharva Lele <itsatharva@gmail.com>
2021-04-17 01:08:12 -0700

    sensor: add bmp280 register definitions
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- components/sensor/bmp280.h
+++ components/sensor/bmp280.h
@@ -1,1 +1,116 @@
-extern void bmp280_init();
+/*
+ * Bosch BMP280 Sensor Header File
+ *
+ * This file includes basic register definitions
+ * and bitmasks that will be required for interfacing
+ * with the sensor.
+ *
+ * The sensor is connected via I2C
+ */
+
+
+#ifndef _BMP280_H_
+#define _BMP280_H_
+
+#include <stdint.h>
+
+/* Debug Switch */
+#define BMP280_DEBUG                    0
+
+/* Chip I2C Address */
+#define BMP280_I2C_ADDR_PRIM            0x76
+#define BMP280_I2C_ADDR_SEC             0x77
+
+/* Chip ID */
+#define BMP280_CHIP_ID                  0x58
+
+/* Register Addresses */
+#define BMP280_CHIP_ID_REG              0xD0
+#define BMP280_RESET_REG                0xE0
+#define BMP280_STATUS_REG               0xF3
+#define BMP280_CTRL_MEAS_REG            0xF4
+#define BMP280_CONFIG_REG               0xF5
+#define BMP280_RAW_VAL_REG_START        0xF7
+#define BMP280_PRESS_MSB_REG            0xF7
+#define BMP280_PRESS_LSB_REG            0xF8
+#define BMP280_PRESS_XLSB_REG           0xF9
+#define BMP280_TEMP_MSB_REG             0xFA
+#define BMP280_TEMP_LSB_REG             0xFB
+#define BMP280_TEMP_XLSB_REG            0xFC
+#define BMP280_MEAS_STATUS_MASK         0x08
+
+/* Power Modes */
+#define BMP280_MODE_MASK                0x03
+#define BMP280_SLEEP                    0x00
+#define BMP280_FORCE                    0x01
+#define BMP280_NORMAL                   0x03
+
+/* Oversampling Macros */
+#define BMP280_OS_NONE                  0x00
+#define BMP280_OS_1X                    0x01
+#define BMP280_OS_2X                    0x02
+#define BMP280_OS_4X                    0x03
+#define BMP280_OS_8X                    0x04
+#define BMP280_OS_16X                   0x05
+#define BMP280_TEMP_OS_MASK             0xE0
+#define BMP280_PRES_OS_MASK             0x1C
+#define BMP280_TEMP_OS_POS              5
+#define BMP280_PRES_OS_POS              2
+
+/* Filter Macros */
+#define BMP280_FILTER_OFF               0x00
+#define BMP280_FILTER_COEFF_2           0x01
+#define BMP280_FILTER_COEFF_4           0x02
+#define BMP280_FILTER_COEFF_8           0x03
+#define BMP280_FILTER_COEFF_16          0x04
+
+/* Compensation Parameter storage */
+#define BMP280_DIG_T1                   0x88
+#define BMP280_DIG_T2                   0x8A
+#define BMP280_DIG_T3					0x8C
+#define BMP280_DIG_P1                   0X8E
+#define BMP280_DIG_P2                   0X90
+#define BMP280_DIG_P3                   0x92
+#define BMP280_DIG_P4                   0x94
+#define BMP280_DIG_P5                   0x96
+#define BMP280_DIG_P6                   0x98
+#define BMP280_DIG_P7                   0x9A
+#define BMP280_DIG_P8                   0x9C
+#define BMP280_DIG_P9                   0x9E
+
+/* Trimming params struct */
+struct bmp280_trim_params {
+    uint16_t dig_T1;
+    int16_t  dig_T2;
+    int16_t  dig_T3;
+
+    uint16_t dig_P1;
+    int16_t  dig_P2;
+    int16_t  dig_P3;
+    int16_t  dig_P4;
+    int16_t  dig_P5;
+    int16_t  dig_P6;
+    int16_t  dig_P7;
+    int16_t  dig_P8;
+    int16_t  dig_P9;
+};
+
+/* Device Struct */
+struct bmp280_device {
+    uint8_t i2c_addr;
+    uint8_t mode;
+    uint8_t temp_os;
+    uint8_t pres_os;
+    uint8_t raw_values[6];
+    uint32_t temperature_raw;
+    uint32_t pressure_raw;
+    int32_t temperature_val;
+    uint32_t pressure_val;
+    int32_t t_fine;
+    struct bmp280_trim_params trim_params;
+};
+
+/* Functions */
+void bmp280_init(struct bmp280_device *bmp280);
+
+#endif
