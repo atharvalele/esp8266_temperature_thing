370b610d2c5a89a139e07dfa9b0b67593a615b6d
Atharva Lele <itsatharva@gmail.com>
2021-04-18 00:23:41 -0700

    main: create sensor reading task
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- main/app_main.c
+++ main/app_main.c
@@ -6,11 +6,26 @@
 
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #include "bmp280.h"
 
 void app_main()
 {
-    struct bmp280_device bmp280;
+    struct bmp280_device bmp280 = {
+        .i2c_addr = BMP280_I2C_ADDR_PRIM,
+        .mode = BMP280_FORCE,
+        .temp_os = BMP280_OS_1X,
+        .pres_os = BMP280_OS_1X,
+};
     
-    bmp280_init(&bmp280);
+    xTaskCreate(
+        bmp280_task,
+        "Sensor Task",
+        1024,
+        &bmp280,
+        1,
+        NULL
+    );
 }
