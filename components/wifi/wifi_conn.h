3d172ed893122cbaa552764e0f5ed4e5edfcc54e
Atharva Lele <itsatharva@gmail.com>
2021-04-23 08:39:00 -0700

    wifi: add simple wifi connection component, taken from ESP examples
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

---
+++ components/wifi/wifi_conn.h
@@ -0,0 +1,9 @@
+/*
+ * WiFi Network Connection Code
+ * 
+ * Taken from the getting started example given at the link below.
+ * All credits to the authors of the repo.
+ * https://github.com/espressif/ESP8266_RTOS_SDK/tree/master/examples/wifi/getting_started/station/main
+ */
+
+extern void wifi_init_sta(void);
